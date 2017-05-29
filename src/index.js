40ff6a2afe388ff7821946f6ac54deff0d07c235
Kristian Mandrup <kmandrup@gmail.com>
2017-05-29 12:22:55 +0200

    enable logging and loose headers

--- src/index.js
+++ src/index.js
@@ -7,85 +7,122 @@ import 'whatwg-fetch'
 import 'url-search-params-polyfill'
 import ES6Promise from 'es6-promise'
 
-export const Fetch = {
-  get(url, query) {
-    if (query) {
-      if (typeof query != 'string') {
-        query = objToSearch(query);
+export function Fetch(opts = {}) {
+  function log(name, ...msgs) {
+    if (opts.logging) {
+      console.log(`Fetch:${name}`, ...msgs)
+    }
+  }
+
+  // const headers = new Headers({
+  //   'content-type': 'application/json'
+  // })
+
+  const headers = {
+    'content-type': 'application/json'
+  }
+
+  return {
+    get(url, query) {
+      if (query) {
+        if (typeof query != 'string') {
+          query = objToSearch(query);
+        }
+        url = `${url}?${query}`
       }
-      url = `${url}?${query}`
-    }
-    return fetch(url, {
-      credentials: 'include'
-    });
-  },
-  post(url, body) {
-    return fetch(url, {
-      method: 'POST',
-      headers: new Headers({
-        'content-type': 'application/json'
-      }),
-      body: JSON.stringify(body),
-      credentials: 'include',
-    })
-  },
-  put(url, body) {
-    return fetch(url, {
-      method: 'put',
-      headers: new Headers({
-        'content-type': 'application/json'
-      }),
-      body: JSON.stringify(body),
-      credentials: 'include',
-    })
-  },
-  patch(url, body) {
-    return fetch(url, {
-      method: 'PATCH',
-      headers: new Headers({
-        'content-type': 'application/json'
-      }),
-      body: JSON.stringify(body),
-      credentials: 'include',
-    })
-  },
-  del(url, query) {
-    if (query) {
-      if (typeof query != 'string') {
-        query = objToSearch(query);
+      let request = {
+        credentials: 'include'
       }
-      url = `${url}?${query}`;
-    }
-    return fetch(url, {
-      method: 'DELETE',
-      credentials: 'include'
-    });
-  },
-  fetch(method, url, query, body) {
-    if (query) {
-      if (typeof query != 'string') {
-        query = objToSearch(query);
+      log('get', {
+        request
+      })
+      return fetch(url, request);
+    },
+    post(url, body) {
+      let request = {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(body),
+        credentials: 'include',
       }
-      url = `${url}?${query}`
+      log('post', {
+        request
+      })
+
+      return fetch(url, request)
+    },
+    put(url, body) {
+      let request = {
+        method: 'put',
+        headers,
+        body: JSON.stringify(body),
+        credentials: 'include',
+      }
+      log('put', {
+        request
+      })
+
+      return fetch(url, request)
+    },
+    patch(url, body) {
+      let request = {
+        method: 'PATCH',
+        headers,
+        body: JSON.stringify(body),
+        credentials: 'include',
+      }
+      log('patch', {
+        request
+      })
+
+      return fetch(url, )
+    },
+    del(url, query) {
+      if (query) {
+        if (typeof query != 'string') {
+          query = objToSearch(query);
+        }
+        url = `${url}?${query}`;
+      }
+      let request = {
+        method: 'DELETE',
+        credentials: 'include'
+      }
+      log('del', {
+        request
+      })
+
+      return fetch(url, request);
+    },
+    fetch(method, url, query, body) {
+      if (query) {
+        if (typeof query != 'string') {
+          query = objToSearch(query);
+        }
+        url = `${url}?${query}`
+      }
+      let options = {
+        method: method.toUpperCase(),
+        credentials: 'include'
+      };
+      if (body) {
+        options.headers = headers;
+        options.body = JSON.stringify(body);
+      }
+      let request = options
+      log('fetch', {
+        request
+      })
+
+      return fetch(url, request);
     }
-    let options = {
-      method: method.toUpperCase(),
-      credentials: 'include'
-    };
-    if (body) {
-      options.headers = new Headers({
-        'content-type': 'application/json'
-      });
-      options.body = JSON.stringify(body);
-    }
-    return fetch(url, options);
   }
 };
 
 export function install(Vue, options = {
   polyfill: true
 }) {
-  Vue.prototype.$fetch = Fetch;
+  Vue.prototype.$fetch = Fetch(options);
   if (options.polyfill) {
     ES6Promise.polyfill();
   }
