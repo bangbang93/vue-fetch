5897dafd3747a4bb034ec32ee58890b1e8936d6c
bangbang93 <bangbang93@163.com>
2026-03-14 21:01:20 +0800

    build: migrate from tsc to tsdown for dual ESM/CJS support
    - Replace tsc with tsdown as build tool
    - Output both ESM (.mjs) and CJS (.cjs) formats
    - Add tsdown.config.ts for build configuration
    - Update package.json exports for dual format support
    - Update test to use Vue 3 API and import from dist
    - Upgrade @types/node to ^20.19.37 for tsdown compatibility

--- test/vue-fetch.ts
+++ test/vue-fetch.ts
@@ -1,18 +1,16 @@
-import Vue from 'vue'
-import VueFetch from '../src/'
+import { createApp } from 'vue'
+import VueFetch from '../dist/index.mjs'
 import 'should'
-
-console.log(require('vue'))
 
 global['window'] = {};
 global['Headers'] = function () {};
 
 describe('vue-fetch', function () {
   it('should install in vue', function () {
-    Vue.use(VueFetch);
-    const vm = new Vue();
-    vm['should'].property('$fetch');
-    vm.$fetch.get['should'].be.Function();
+    const app = createApp({});
+    app.use(VueFetch);
+    app.config.globalProperties['should'].property('$fetch');
+    app.config.globalProperties.$fetch.get['should'].be.Function();
   });
   it('should can be used in other contexts', function () {
     const fetch = VueFetch();
